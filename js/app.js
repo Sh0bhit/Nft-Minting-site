6a405c148ca965086a0e83a2b4b44d75878c9c1f
sh0bhit <sguptamds94@gmail.com>
2021-12-22 00:14:08 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 const buttons = document.querySelector(".mintbtn");
+const nav = document.querySelectorAll(".btn");
 const mint = document.querySelector(".mint");
+const s = document.querySelectorAll(".d");
 
 buttons.addEventListener('click', function(){
-	console.log("Hemlo");
     mint.classList.toggle("show");
 })
+
+
+function chan(){
+    let index = this.dataset.value;
+    let i = 1;
+     
+    let r = document.querySelector(`.div-${index}`); 
+    for(i=1;i<=4;i++){
+        if(i!=index){    
+        let f = document.querySelector(`.div-${i}`);
+        f.classList.remove("on");
+        
+
+        }
+        else{
+            let d = document.querySelector(`.div-${index}`);
+            d.classList.add("on");
+            console.log(i);
+        }
+       
+    }
+
+}
+
+nav.forEach((n) =>{
+    n.addEventListener('click', chan);
+
+});
+
+
+
